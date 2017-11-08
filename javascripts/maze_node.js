1fbe1c613471eda58b5e82510b02669408388b21
Aarron Lee <aclee91@gmail.com>
2017-11-08 17:24:40 -0500

    add print() function, TODO: refactor with CSS classes

--- javascripts/maze_node.js
+++ javascripts/maze_node.js
@@ -17,6 +17,25 @@ class MazeNode{
     }
     return null;
   }
+
+  print(){
+    let style = "";
+    if(this.walls["N"]){
+      style+= " border-top: 1px solid black;";
+    }
+    if(this.walls["S"]){
+      style+= " border-bottom: 1px solid black;";
+    }
+    if(this.walls["E"]){
+      style+= " border-right: 1px solid black;";
+    }
+    if(this.walls["W"]){
+      style+= " border-left: 1px solid black;";
+    }
+
+    return `<div class="maze-node" id="node-${this.pos[0]}-${this.pos[1]}" style="${style}"></div>`
+  }
+
 }
 
 export default MazeNode;
