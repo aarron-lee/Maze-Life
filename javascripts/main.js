fb988df9f298d96b98af238dcee968e7049cefe3
Aarron Lee <aclee91@gmail.com>
2017-11-19 12:33:52 -0500

    modals w/ close button added

--- javascripts/main.js
+++ javascripts/main.js
@@ -64,6 +64,42 @@ let handleSearchExtras = (maze, buttonsToEnable) => {
 
 }
 
+let handleModalButtons = () =>{
+  let generatorsModalButton = document.getElementById('#generators-modal');
+  let solversModalButton = document.getElementById('#solvers-modal');
+  let modalDiv = document.querySelector('#modal-location');
+
+  generatorsModalButton.addEventListener("click", (e) =>{
+    e.preventDefault();
+    let html = '<div class="modal-backdrop"><div class="modal-inner"><h2>Maze Generators</h2>';
+    html+= '<p>These buttons are used to generate different grids, which are then used to test the Maze Solvers algorithms</p>';
+    html+='<div class="close-modal">X</div>';
+    html+= '</div></div>';
+    modalDiv.innerHTML = html;
+    document.querySelector('.modal-backdrop').addEventListener("click", (e) =>{
+      e.preventDefault();
+      modalDiv.innerHTML = '';
+    });
+  });
+
+  solversModalButton.addEventListener("click", (e) =>{
+    e.preventDefault();
+    let html = '<div class="modal-backdrop"><div class="modal-inner"><h2>Maze Solvers</h2>';
+    html+= '<p><h2>Description</h2>These buttons are used to find a path from the start point to the end point indicated on the grid</p>';
+    html+= '<p><h2>DFS</h2> Depth First Search Algorithm. This algorithm traverses a path until it finds a dead end, then backtracks to the next fork where it can continue. Rinse, repeat, until a path is found.</p>';
+    html+= '<p><h2>BFS</h2> Breadth First Search Algorithm. This algorithm traverses a path until it reaches a fork, and at each fork, it splits up and traverses down each path evenly. Rinse, repeat, until a path is found.</p>';
+    html+= '<p><h2>A*</h2> A* search algorithm. This algorithm uses a heuristic to keep track of estimated distance to the goal location, and traverses the grid based on the shortest distances indicated by the heuristic.</p>';
+    html+='<div class="close-modal">X</div>';
+    html+= '</div></div>';
+    modalDiv.innerHTML = html;
+    document.querySelector('.modal-backdrop').addEventListener("click", (e) =>{
+      e.preventDefault();
+      modalDiv.innerHTML = '';
+    });
+  });
+
+
+}
 
 document.addEventListener("DOMContentLoaded", () =>{
 
@@ -81,8 +117,8 @@ document.addEventListener("DOMContentLoaded", () =>{
   window.maze = new MazeGrid(45);
 
   handleMazeExtras(maze, buttons);
-
   handleSearchExtras(maze, buttons);
+  handleModalButtons();
 
   maze.generateMaze(true, []);
 
